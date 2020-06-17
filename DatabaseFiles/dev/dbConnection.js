ee55afc45de9ae1925467ee4422a65a4298a6d38
Fadel Sobh <fx1057@Wayne.edu>
2020-06-17 17:10:00 -0400

    New Endpoints added

--- DatabaseFiles/dev/dbConnection.js
+++ DatabaseFiles/dev/dbConnection.js
@@ -18,9 +18,23 @@ const createAnimalsTable = () => {
   pool.query(animalCreateQuery) //--> tell pool to execut the query
     .then((res) => {
       console.log(res);
+      
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+
+    const insert = "INSERT INTO ANIMALS(animal_name, animal_type, animal_age) VALUES(Fred, Elephant, 10), (geany, Lion, 10), (Kevin, Dog, 8);"
+
+    pool.query(insert) //--> tell pool to execut the query
+    .then((res) => {
+      console.log("Data inserted....")
+      console.log(res);
       pool.end();
     })
     .catch((err) => {
+      console.log("Data NOT inserted....")
       console.log(err);
       pool.end();
     });
--- app.js
+++ app.js
@@ -1,30 +1,72 @@
-import express from 'express'
+import express, { response } from 'express'
 import dbQuery from './DatabaseFiles/dev/dbQuery';
-import {createAnimalsTable} from './DatabaseFiles/dev/dbConnection'
+import { createAnimalsTable } from './DatabaseFiles/dev/dbConnection';
+
 
 const PORT = 3000
 const server = express();
 
+//generate URL for each endpoint
 const constructURL = (version, urlPath) => `/api/${version}/${urlPath}`
 
-server.get(constructURL('v1','list'),(req, res) => {
-    
-   // createAnimalsTable()
-    
-    const all = dbQuery.query("SELECT * FROM ANIMALS")
+//Get all animals
+server.get(constructURL('v1','list_animals'),(req, response) => {
 
-    res.json(all)
-        
+     dbQuery.query("SELECT * FROM animals").then((res) => {
+
+        response.send(JSON.stringify(res.rows))
+      
+    })
 })
 
-server.listen(PORT,  () => {
+//Get animal by id 
+server.get(`${constructURL('v1','list_animal')}/:id`,(req, response) => {
+
+    const animal_id = req.params.id
+
+    dbQuery.query("SELECT * FROM animals WHERE ID = $1", [animal_id]).then((res) => {
+
+        res ?
+    res.rowCount > 0 ? response.send(JSON.stringify(res.rows)) : response.send("Animal Does not exist") : res.send("Resource Error")
+
+       
+     
+   })
+})
+
+//Add an animal
+server.post(constructURL('v1','update_animal'),(req, response) => {
+
+    
+    response.end()
+
+})
+
+//Update an animal by ID
+server.put(constructURL('v1','add_animal'),(req, response) => {
+
+    
+    response.end()
+
+})
+
+//remove an animal
+server.delete(constructURL('v1','remove_animal'),(req, response) => {
+
+    
+    response.end()
+
+})
+
+server.listen(PORT, () => {
 
     try{
         console.log(`server started on port ${PORT} `)
-           createAnimalsTable() //Table was successfully created !
+         
     }
     catch (error){
 
+        
         console.log(error)
 
     }
