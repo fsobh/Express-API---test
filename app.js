ac6ed5b9b1ac5ee9df59cdcb0e279b793fee53c5
Fadel <fx1057@wayne.edu>
2020-06-19 10:16:02 -0400

    Dev env.....

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
-import express, { response } from 'express'
-import AnimalRoute from './routes/animalRoute'
-import bodyparser from 'body-parser'
-import morgan from 'morgan'// middleware dependency
+const express = require("express"); //import express
+const massive = require("massive"); //import massive as the access tool were using to communicate back and forth with the database
+const animalRoute = require("./routes/animalRoute"); //animal routes
+const PORT = 3000; /*Testing on port 3000 */
+require("dotenv").config();
 
-//Ask Greg Why calling this at any endpoint causes the request to fail.
-import { createAnimalsTable } from './DatabaseFiles/dev/dbConnection';
+const app = express(); /* Standard */
 
+app.use(express.json()); // requests will use json format
 
-//Local testing is happening on port 3000
-const PORT = 3000
-const server = express();
+const constructURL = (version, urlPath) => `/api/${version}/${urlPath}`;
 
-//generate URL for each endpoint using the version and path strings passed in 
-const constructURL = (version, urlPath) => `/api/${version}/${urlPath}`
+// set routers here //
+//app.use(constructURL("v1", "animals"), animalRoute); //set animals route
 
-server.use(morgan('tiny')) //middleware dependency
-server.use(bodyparser.json()) // data in the body in json format
+app.set("/", (req, res) => {
+  res.sendStatus(200);
+});
 
-server.use(constructURL('v1','animals'), AnimalRoute) // tell the serever that requests with the following url
-// (/api/v1/animals) are to be routed to the Animal router file where the endpoints were declared. The router file was imported at the top
-
-
-
-server.listen(PORT, () => {
-
-    try{
-        console.log(`server started on port ${PORT} `)
-         
-    }
-    catch (error){
-
-        
-        console.log(error)
-
-    }
-    
-    
-    
-
+massive({
+  database: process.env.DATABASE, // database name
+  host: process.env.DB_HOST, //host
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
 })
+  .then((database) => {
+    app.set("database", database);
+    console.log("Connected to Remote Database");
+
+    module.exports = database; // ask greg why we did this
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//establish connection
+app.listen(PORT, () => {
+  try {
+    console.log(`server started on port ${PORT} `);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// //generate URL for each endpoint using the version and path strings passed in
+
+// server.use(morgan("tiny")); //middleware dependency
+// server.use(bodyparser.json()); // data in the body in json format
+
+// server.use(constructURL("v1", "animals"), AnimalRoute); // tell the serever that requests with the following url
+// // (/api/v1/animals) are to be routed to the Animal router file where the endpoints were declared. The router file was imported at the top
--- routes/animalRoute.js
+++ routes/animalRoute.js
@@ -1,91 +1,82 @@
-/**
- * @Description : These are the different routes that are available for accessing, modifying and deleting Animals from the database
- */
+// /**
+//  * @Description : These are the different routes that are available for accessing, modifying and deleting Animals from the database
+//  */
 
-import express from 'express'
-import dbQuery from '../DatabaseFiles/dev/dbQuery';
-import _ from  'lodash'
+// import express from "express";
 
+// import _ from "lodash";
 
-const router = express.Router();
+// const router = express.Router();
 
+// /**Get all animals
+//  *Request Type : GET
+//  * URL : http://localhost:3000/api/v1/list_animals
+//  */
+// router.get("/", (req, response) => {
+//   dbQuery.query("SELECT * FROM animals").then((res) => {
+//     response.send(JSON.stringify(res.rows));
+//   });
+// });
 
-/**Get all animals
-*Request Type : GET
-* URL : http://localhost:3000/api/v1/list_animals
-*/
-router.get('/',(req, response) => {
+// /*Get animal by id (using a URL parameter)
+//  * Request Type : GET
+//  * URL : http://localhost:3000/api/v1/animals/<animal_id>
+//  */
+// router.get("/:id", (req, response) => {
+//   const animal_id = req.params.id;
 
-    dbQuery.query("SELECT * FROM animals").then((res) => {
+//   dbQuery
+//     .query("SELECT * FROM animals WHERE ID = $1", [animal_id])
+//     .then((res) => {
+//       res
+//         ? res.rowCount > 0
+//           ? response.send(JSON.stringify(res.rows))
+//           : response.send("Animal Does not exist")
+//         : res.send("Resource Error");
+//     });
+// });
 
-       response.send(JSON.stringify(res.rows))
-     
-   })
-})
+// /*update animal by id (using request params)
+//  * Request Type : POST
+//  * URL : http://localhost:3000/api/v1/animals
+//  */
+// router.post("/", (req, response) => {
+//   if (_.isEmpty(req.body)) {
+//     //if no data was provided in the body,
+//     response.send("No data provided in the body");
+//     response.end();
+//   }
 
-/*Get animal by id (using a URL parameter)
-* Request Type : GET
-* URL : http://localhost:3000/api/v1/animals/<animal_id>
-*/
-router.get('/:id',(req, response) => {
+//   dbQuery
+//     .query(
+//       "UPDATE animals SET animal_name = $1, animal_type = $2 , animal_age = $3  WHERE ID = $4",
+//       [req.body.name, req.body.type, req.body.age, animal_id]
+//     )
+//     .then((res) => {
+//       res
+//         ? res.rowCount > 0
+//           ? response.send(JSON.stringify(res.rows))
+//           : response.send("Animal Does not exist")
+//         : res.send("Resource Error");
+//     });
+// });
 
-   const animal_id = req.params.id
+// /*add a new animal (using request params)
+//  * Request Type : PUT
+//  * URL : http://localhost:3000/api/v1/animals
+//  */
+// router.put("/", (req, response) => {
+//   response.end();
+// });
 
-   dbQuery.query("SELECT * FROM animals WHERE ID = $1", [animal_id]).then((res) => {
+// //remove an animal
+// router.delete("/", (req, response) => {
+//   console.log("delete_Request");
+//   response.end();
+// });
 
-       res ?
-   res.rowCount > 0 ? response.send(JSON.stringify(res.rows)) : response.send("Animal Does not exist") : res.send("Resource Error")
+// router.param("id", (req, res, next, id) => {
+//   isNaN(id) ? next(`Invalid parameter data type : ${id}`) : next();
+// });
 
-       
-  })
-})
-
-/*update animal by id (using request params)
-* Request Type : POST
-* URL : http://localhost:3000/api/v1/animals
-*/
-router.post('/',(req, response) => {
-
-    if(_.isEmpty(req.body)){ //if no data was provided in the body, 
-        response.send("No data provided in the body")
-        response.end()
-    }
-
-    dbQuery.query("UPDATE animals SET animal_name = $1, animal_type = $2 , animal_age = $3  WHERE ID = $4", [req.body.name,req.body.type,req.body.age,animal_id]).then((res) => {
-
-        res ?
-    res.rowCount > 0 ? response.send(JSON.stringify(res.rows)) : response.send("Animal Does not exist") : res.send("Resource Error")
- 
-        
-   })
-
-
-   
-
-})
-
-/*add a new animal (using request params)
-* Request Type : PUT
-* URL : http://localhost:3000/api/v1/animals
-*/
-router.put('/',(req, response) => {
-
-   
-   response.end()
-
-})
-
-//remove an animal
-router.delete('/',(req, response) => {
-
-   console.log("delete_Request")
-   response.end()
-
-})
-
-router.param('id',(req, res, next, id) => {
-
-    isNaN(id) ? next(`Invalid parameter data type : ${id}`) : next()
-})
-
-module.exports = router;
+// module.exports = router;
